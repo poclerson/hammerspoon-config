76de95dbd9287c5e72ead7ae381792720072055f
poclerson <cosmoscosmos993@gmail.com>
2023-08-26 04:02:03 -0400

    doesn't throw errors when application is nil on window move

--- Spoons/WindowManager.spoon/init.lua
+++ Spoons/WindowManager.spoon/init.lua
@@ -1,24 +1,35 @@
 IsWindowHeld = false
 
+-- Maximizes window while taking stage manager into account
 local function maximizeWindow(window)
+  if (not window) then
+    return
+  end
   local max = window:screen():frame()
 
   window:setFrame({x=max.x, y=max.y, w=max.w * 92/100, h=max.h})
 end
 
--- Redimenssionne toutes les applications à la bonne taille de l'écran
+-- Applications events listener
 local function watchApplications(name, event)
+  local app = hs.application.get(name)
+  if (event == hs.application.watcher.launched) then
+    maximizeWindow(app:focusedWindow())
+  end
   if (event == hs.application.watcher.activated) then
-    local app = hs.application.get(name)
-
-    local leftClickDownWatcher = hs.eventtap.new({hs.eventtap.event.types.leftMouseDown}, function() IsWindowHeld = true end)
+    local leftClickDownWatcher = hs.eventtap.new(
+      {hs.eventtap.event.types.leftMouseDown},
+      function() IsWindowHeld = true end
+    ) 
     leftClickDownWatcher:start()
   end
 end
 
+-- Keyboard/mouse events listener
 local function watchEvents(event) 
   if (IsWindowHeld) then
     maximizeWindow(hs.application.frontmostApplication():focusedWindow())
+    IsWindowHeld = false
   end
 end
 
