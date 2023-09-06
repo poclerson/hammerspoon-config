6864cb7567cc059b0d334835649c5245f5eaf7f3
poclerson <cosmoscosmos993@gmail.com>
2023-09-05 23:06:40 -0400

    app watcher doesn't get garbage collected

--- init.lua
+++ init.lua
@@ -8,3 +8,17 @@ local spoons = {
 for _, spoon in pairs(spoons) do
   hs.loadSpoon(spoon):start()
 end
+
+local function onApplicationEvent(name, event)
+  spoon.Window:watchApplications(name, event)
+end
+
+applicationWatcher = hs.application.watcher.new(onApplicationEvent)
+
+applicationWatcher:start()
+
+function reloadConfig(paths, force)
+  print('Config reloaded')
+  applicationWatcher:stop()
+  applicationWatcher = nil
+end
