a4bb389372ef3e6c88a47d8138b6d065a9a242e1
poclerson <cosmoscosmos993@gmail.com>
2023-12-13 01:01:53 -0500

    handle state function simplification

--- Spoons/Switcher.spoon/lib/utils/handle-state.lua
+++ Spoons/Switcher.spoon/lib/utils/handle-state.lua
@@ -1,3 +1,12 @@
+---@param activatedKeyCode string|table
+---@param keycode string
+---@param actionName Switcher.Actions
+---@param isOpen boolean
+---@return boolean
+local function shouldActivateAction(activatedKeyCode, keycode, actionName, isOpen)
+  return keycode == activatedKeyCode and (actionName == 'selectNext' or isOpen)
+end
+
 ---Handles what action to call when keyboard events related to the app switcher are called. Is called on every keyup and keydown
 ---@param event hs.eventtap.event
 function switcher:handleState(event)
@@ -16,59 +25,56 @@ function switcher:handleState(event)
   end
 
   local eventType = event:getType()
-  local keycode = event:getKeyCode()
+  local keycode = event:getKeyCode() --[[@as string]]
 
-  if eventType == hs.eventtap.event.types.keyDown then
-    local selectedApp = self:getSelectedApp()
-    local blockDefault = false
+  local selectedApp = self:getSelectedApp()
+  local blockDefault = false
 
-    ---@param activatedKeyCode string|table
-    ---@param actionName Switcher.Actions
-    ---@return boolean
-    local function shouldActivateAction(activatedKeyCode, actionName)
-      return keycode == activatedKeyCode and (actionName == 'selectNext' or self.isOpen)
+  if eventType ~= hs.eventtap.event.types.keyDown then
+    return blockDefault
+  end
+
+  hs.fnutils.eachPair(self.actions, function (name, fn)
+    local action = self.keybinds[name]
+
+    -- If the key is directly assigned to the action
+    if shouldActivateAction(action, keycode, name, self.isOpen) then
+      fn(selectedApp)
+      blockDefault = true
+      return
     end
 
-    hs.fnutils.eachPair(self.actions, function (name, fn)
-      local action = self.keybinds[name]
+    -- If the action is nested, it has either action parameters or multiple available key binds
+    if type(action) ~= 'table' then
+      return
+    end
 
-      -- If the key is directly assigned to the action
-      if shouldActivateAction(action, name) then
-        fn(selectedApp)
+    hs.fnutils.eachPair(action, function (fnParameter, fnKeyCode)
+      -- If the action has action parameters
+      if shouldActivateAction(fnKeyCode, keycode, name, self.isOpen) then
+        fn(selectedApp, fnParameter)
         blockDefault = true
         return
       end
 
-      -- If the action is nested, it has either action parameters or multiple available key binds
-      if type(action) == 'table' then
-        hs.fnutils.eachPair(action, function (fnParameter, fnKeyCode)
-
-          -- If the action has action parameters
-          if shouldActivateAction(fnKeyCode, name) then
-            fn(selectedApp, fnParameter)
-            blockDefault = true
-            return
-          end
-
-          -- If the action has multiple key binds
-          -- OR
-          -- If the action parameter has multiple key binds
-          if fnParameter == '__keybinds' or (type(fnKeyCode) == 'table' and fnKeyCode['__keybinds']) then
-
-            -- Choose the table to iterate through depending on the condition
-            local keyCodes = fnParameter == '__keybinds' and fnKeyCode or fnKeyCode['__keybinds']
-            hs.fnutils.each(keyCodes, function (optionnalKeyCode)
-              if shouldActivateAction(optionnalKeyCode, name) then
-                fn(selectedApp, fnParameter)
-                blockDefault = true
-                return
-              end
-            end)
-            return
-          end
-        end)
+      -- If the action has multiple key binds
+      -- OR
+      -- If the action parameter has multiple key binds
+      if fnParameter ~= '__keybinds' and not (type(fnKeyCode) == 'table' and fnKeyCode['__keybinds']) then
+        return
       end
+
+      -- Choose the table to iterate through depending on the condition
+      local keycodes = fnParameter == '__keybinds' and fnKeyCode or fnKeyCode['__keybinds']
+
+      hs.fnutils.each(keycodes, function (optionnalKeyCode)
+        if shouldActivateAction(optionnalKeyCode, keycode, name, self.isOpen) then
+          fn(selectedApp, fnParameter)
+          blockDefault = true
+          return
+        end
+      end)
     end)
-    return blockDefault
-  end
+  end)
+  return blockDefault
 end
